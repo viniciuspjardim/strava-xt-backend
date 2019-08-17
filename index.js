74704f7e8b20c5531ce11c9b9f0cd5762d7733b4
Vinícius Jardim <viniciuspjardim@gmail.com>
2019-08-17 19:45:41 -0300

    Allowing access (CORS)

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const activities = require('./routes/activities');
 
 const app = express();
 
+// CORS: Allowing access from http://localhost:8080
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use('/', home);
-app.use('/api/activities', activities)
+app.use('/api/activities', activities);
 
 // How to set a env (PORT) var in Windows PowerShell: $env:PORT = 5000
 const PORT = process.env.PORT || 3000;
